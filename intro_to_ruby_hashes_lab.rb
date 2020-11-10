9a325635c7a747d49111d06200eac44c6f12d484
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-10 01:31:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	{:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
+	{:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {:one_piece_owned => 25}}}
 end
 
 def monopoly_with_third_tier
